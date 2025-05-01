a2fc9529b15accaf1e4aa9414cabbb9857e63416
rackovicb <rackovicb@gmail.com>
2025-05-01 14:35:32 +0200

    User registration, Middleware, Log In and Log Out

--- controllers/index.php
+++ controllers/index.php
@@ -1,7 +1,5 @@
 <?php
 
-$_SESSION['name'] = 'Bosko';
-
 view("index.view.php",[
     'heading' => 'Home'
 ]);
--- routes.php
+++ routes.php
@@ -1,20 +1,12 @@
 <?php
 
-// return [
-//     '/' => 'controllers/index.php',
-//     '/about' => 'controllers/about.php',
-//     '/real_estates' => 'controllers/real_estates/index.php',
-//     '/real_estate' => 'controllers/real_estates/show.php',
-//     '/real_estates/create' => 'controllers/real_estates/create.php',
-//     '/contact' => 'controllers/contact.php',
-// ];
 
 $router->get('/', 'controllers/index.php');
 $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
 
 
-$router->get('/real_estates', 'controllers/real_estates/index.php');
+$router->get('/real_estates', 'controllers/real_estates/index.php')->only('auth');
 $router->get('/real_estate', 'controllers/real_estates/show.php');
 $router->delete('/real_estate', 'controllers/real_estates/destroy.php');
 
@@ -25,4 +17,10 @@ $router->patch('/real_estate', 'controllers/real_estates/update.php');
 $router->get('/real_estates/create', 'controllers/real_estates/create.php');
 $router->post('/real_estates', 'controllers/real_estates/store.php');
 
+$router->get('/register', 'controllers/registration/create.php')->only('guest');
+$router->post('/register', 'controllers/registration/store.php')->only('guest');
 
+
+$router->get('/login', 'controllers/session/create.php')->only('guest');
+$router->post('/session', 'controllers/session/store.php')->only('guest');
+$router->delete('/session', 'controllers/session/destroy.php')->only('auth');
